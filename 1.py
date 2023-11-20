b595e249fea0ada27e9b91c6c05c727710928f3c
789sw <113761212+789sw@users.noreply.github.com>
2023-11-20 22:07:04 +0800

    完成資料輸入輸出，差判斷等差等比

--- 1.py
+++ 1.py
@@ -2,5 +2,16 @@
 
 t = input()
 t_list = []
-for t_list in range(t) :
-    
+for i in range(t) :
+    for j in range(4):
+        t_list[i][j] = input()
+for i in range(t):
+    for j in range(4):
+        print (t_list[i][j],end = " ") 
+    print("\n")
+#其中end=",意思是为末尾end传递一个空字符串，
+# 这样print函数不会在字符串末尾添加一个换行符，
+# 而是添加一个空字符串，其实这也是一个语法要求，
+# 表示这个语句没结束。
+#print默认是打印一行，结尾加换行。end=’ '意思是末尾不换行，加空格。
+
